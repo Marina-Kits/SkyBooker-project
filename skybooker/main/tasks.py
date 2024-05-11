60f4b4535ae2173e8efaddffba4b3714a2ef2c90
polya <polyakrupina@gmail.com>
2024-05-11 20:10:13 +0500

    update deleting tickets

--- skybooker/main/tasks.py
+++ skybooker/main/tasks.py
@@ -5,13 +5,11 @@ from .models import Ticket, FlightClassInfo
 
 @shared_task
 def delete_expired_tickets():
-    print('aaaaaa')
-    expiration_time = datetime.now() - timedelta(minutes=1)
+    expiration_time = datetime.now() - timedelta(minutes=15)
 
     expired_tickets = Ticket.objects.filter(is_confirmed=False, booking_time__lte=expiration_time)
 
     for ticket in expired_tickets:
-        print('i deleted smth')
         flight_class_info = FlightClassInfo.objects.get(flight=ticket.flight, service_class=ticket.service_class)
         flight_class_info.seats_number += 1
         flight_class_info.save()
